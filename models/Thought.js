32ce8d636faca15d362a9d9328d0ca007178eaa7
Sam Mina <samandmissy2010@gmail.com>
2024-11-28 16:29:26 -0500

    models Thought js

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 500,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual property that gets number of reactions.
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+// Thought model using thoughtSchema.
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
